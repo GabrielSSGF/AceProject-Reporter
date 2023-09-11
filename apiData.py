4c1452590717441b786186e912648c547849fa68
Gabriel Soares da Silva Góes <gabriel_soares1125@outlook.com>
2023-09-11 14:20:57 -0300

    Fixed a error with the DADOS_FROM_JSON

--- apiData.py
+++ apiData.py
@@ -6,11 +6,11 @@ import json
 
 class APIData:
     def __init__(self):
-        global DATE_CLASS, DADOS_FROM_JSON
+        global DATE_CLASS
         DATE_CLASS = dateSettings.DateSettings()
-        DADOS_FROM_JSON = self._getDataFromFile()
-        self.GUID = self._getGuid()
-        self.FULLDISPLAY_USERNAMES = self._getUserNames(self.GUID)
+        self.DADOS_FROM_JSON = self._getDataFromFile()
+        self.GUID = self._getGuid(self.DADOS_FROM_JSON)
+        self.FULLDISPLAY_USERNAMES = self._getUserNames(self.GUID, self.DADOS_FROM_JSON)
         self.LOWERCASE_USERNAMES = self._getLowerCaseUserNames(self.FULLDISPLAY_USERNAMES)
 
     def _getDataFromFile(self):
@@ -18,7 +18,7 @@ class APIData:
             dados = json.load(arquivoJson)
         return dados
 
-    def _getGuid(self):
+    def _getGuid(self, DADOS_FROM_JSON):
         dadosFromAPI = self._requestAPI(f"https://api.aceproject.com/?fct=login&accountid={DADOS_FROM_JSON['AccountId']}&username={DADOS_FROM_JSON['UserName']}&password={DADOS_FROM_JSON['Password']}&format=json")
         guid = dadosFromAPI['results'][0]['GUID']
         return guid
@@ -35,20 +35,22 @@ class APIData:
         dadosFromAPI = self._requestAPI(f"https://api.aceproject.com/?fct=gettimeSheetstoapprove&guid={guid}&projectid=NULL&filtertimeSheetperiodid=NULL&filtertimeSheetlineid=NULL&filterdatefrom={dataInicio}&filterdateto=NULL&filtertimestatus=NULL&filterprojectstatus=NULL&filterprojecttypeid=NULL&filterusergroupid=NULL&filterclientid=NULL&filteruserid=NULL&filtertimetypeid=NULL&weekview=False&sortorder=NULL&pagenumber=NULL&rowsperpage=NULL&format=json")
         return dadosFromAPI
 
-    def _getUserNames(self, guid):
-        userNames = self._getAllUserNames(guid)
+    def _getUserNames(self, guid, DADOS_FROM_JSON):
+        userNames = self._getAllUserNames(guid, DADOS_FROM_JSON)
         return userNames
 
-    def _getAllUserNames(self, guid):
+    def _getAllUserNames(self, guid, DADOS_FROM_JSON):
         dadosFromAPI = self._requestAPI(f"https://api.aceproject.com/?fct=getusersstats&guid={guid}&filteractive=True&format=json")
         allUsersFullDisplayName = [entry["DISPLAYED_NAME"] for entry in dadosFromAPI["results"]]
-        userFullDisplayName = self._removeManagerUserNames(allUsersFullDisplayName)
+        userFullDisplayName = self._removeManagerUserNames(allUsersFullDisplayName, DADOS_FROM_JSON)
         return userFullDisplayName
     
-    def _removeManagerUserNames(self, userFullDisplayName):
-        managersUserNames = [DADOS_FROM_JSON["ManagersToRemoveFromBilling"]]
+    def _removeManagerUserNames(self, userFullDisplayName, DADOS_FROM_JSON):
+        managersUserNames = DADOS_FROM_JSON["ManagersToRemoveFromBilling"]
+        print(managersUserNames)
         for i in managersUserNames:
-            userFullDisplayName.remove(i)
+            if i in userFullDisplayName:
+                userFullDisplayName.remove(i)
         return userFullDisplayName
     
     def _getLowerCaseUserNames(self, userNames):
